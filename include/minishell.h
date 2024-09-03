54f17637228688523843f9f5fee6cf2cdfbd2b45
raphael <raphael.carbonnnel@gmail.com>
2024-09-03 17:46:40 +0200

    new

---
+++ include/minishell.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raphaelcarbonnel <raphaelcarbonnel@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 15:12:37 by raphaelcarb       #+#    #+#             */
+/*   Updated: 2024/09/03 16:56:07 by raphaelcarb      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef minishell.h
+#define minishell.h
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <termios.h>
+#include <limits.h>
+#include <curses.h>
+#include <term.h>
+
+
+// parsing 
+
+
+// utils 
+
+
+// token
+
+
+// pipe
+
+
+
+#endif
