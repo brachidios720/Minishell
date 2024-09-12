a385a12f6849a3e2d61c7167b6da2ab7fcc10950
PAG <stephpagliardini@gmail.com>
2024-09-12 17:12:42 +0200

    add ft_echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -12,6 +12,26 @@
 
 #include "../../include/minishell.h"
 
+bool	echo_n(char *argv)
+{
+	int	i;
+
+	if (argv[0] != '-' && argv[1] != 'n')
+	{
+		return (false);
+	}
+	i = 1;
+	while (argv[i])
+	{
+		if (argv[i] != 'n')
+		{
+			return (false);
+		}
+		i++;
+	}
+	return (true);
+}
+
 void	ft_echo(char **argv)
 {
 	int		i;
@@ -19,22 +39,26 @@ void	ft_echo(char **argv)
 
 	i = 1;
 	new_line = true;
-	if (argv[1] && strcmp(argv[1], "-n") == 0)
+	while (argv[i] && echo_n(argv[i]))
 	{
 		new_line = false;
 		i++;
 	}
 	while (argv[i])
 	{
+		//if(argv[i] + 1)
+		//{
+		//	write (1, " ", 1);
+		//}
 		write (1, argv[i], strlen(argv[i]));
-		if (argv[i + 1])
-		{
-			write(1, " ", 1);
-		}
 		i++;
 	}
 	if (new_line)
 	{
-		write(1, "\n", 1);
+		write (1, "\n", 1);
 	}
 }
+//echo sans options -> ajoute un saut de ligne par defaut
+//echo -n -> supprime le saut de ligne
+//echo -nnnnn -> marche comme -n
+//echo --n -> cf parsing
