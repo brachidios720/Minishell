081ad10f7aa8ad203deb0f83472fcdab3cd132da
PAG <stephpagliardini@gmail.com>
2024-09-12 17:11:31 +0200

    add unset.c

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -12,18 +12,22 @@
 
 #include "../../include/minishell.h"
 
-void      ft_check_bultins(char *line, char **env)
+void	ft_check_bultins(char *line, char **env)
 {
-    if(line == NULL || line[0] == '\0')
-        return;
-    char **arg = ft_split(line, ' ');
-    if(ft_strcmp(arg[0], "env") == 0)
-        ft_env(env);
-    else if(ft_strcmp(arg[0], "pwd") == 0)
-        ft_pwd();
-    else if(ft_strcmp(arg[0], "echo") == 0)
-        ft_echo(arg);
-    else 
-        write(1, "command not found\n", 19);
-    free(arg);
+	char	**arg;
+
+	if (line == NULL || line[0] == '\0')
+		return ;
+	arg = ft_split(line, ' ');
+	if (ft_strcmp(arg[0], "env") == 0)
+		ft_env(env);
+	else if (ft_strcmp(arg[0], "pwd") == 0)
+		ft_pwd();
+	else if (ft_strcmp(arg[0], "echo") == 0)
+		ft_echo(arg);
+	else if (ft_strcmp(arg[0], "unset") == 0)
+		ft_unset(arg, env);
+	else
+		write (1, "command not found\n", 19);
+	free(arg);
 }
