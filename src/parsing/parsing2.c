1b2245415e204523d532a689512afeecf85d434d
brachidios720 <raphael.carbonnnel@gmail.com>
2024-10-14 16:26:37 +0200

    dd

--- src/parsing/parsing2.c
+++ src/parsing/parsing2.c
@@ -6,17 +6,11 @@ int ft_check_pipe(char *str)
     while(str[i] == ' ' && str[i])
         i++;
     if(str[i] == '|')
-    {
-        printf(RED"error\n"RESET);
         return(1);
-    }
     while(str[i])
         i++;
     if(str[i - 1] == '|')
-    {
-        printf(RED"error\n"RESET);
         return(1);
-    }
     return(0);
 }
 
