6d95d6c891984a108af8ea04ca5a8fb4da3f4fea
brachidios720 <raphael.carbonnnel@gmail.com>
2024-10-29 09:45:04 +0100

    yess

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -12,113 +12,98 @@
 
 #include "../../include/minishell.h"
 
-//trouve le chemin de la commande
-char *find_command_path(char *cmd)
+//prepare le tableau argv en fonction des infos donnees dans cmd
+char **prepare_argv(t_cmd *cmd, char *cmd_path)
 {
-	char *path; //stock la valeur de la var d'env PATH
-	char **paths; //tab de chaines de car qui stock les rep separes par des ":" a partir de PATH
-	char *full_path; //stock le chemin complet de la commande -> concatene le rep et la commande
-	int i; //parcourt les repertoires dans paths
-
-	path = getenv("PATH");
-	paths = ft_split(path, ':');
-	i = 0;  
-
-	while (paths[i])
+	char **argv = malloc(sizeof(char *)*3); //3 elements
+	if (!argv)
 	{
-		full_path = ft_strjoin(paths[i], "/");
-		full_path = ft_strjoin(full_path, cmd);
-		if (access(full_path, X_OK) == 0)
-			return full_path;
-		i++;
+		return (NULL);
 	}
-	return NULL;
+	argv[0] = cmd_path; //ls
+	argv[1] = cmd->option; //-la
+	argv[2] = NULL;
+
+	return (argv);
 }
 
-void handle_redir(t_cmd *cmd)
+//gestion de l execution d une commande avec les redirections et la variables d environnement
+void exec_with_env_and_redir(t_cmd *cmd, t_data *data)
 {
-	// Gestion de redirections d'entree et de sortie
-	if (cmd->infile)
-	{
-		int fd_in;
-		fd_in = open(cmd->infile, O_RDONLY);
-		if (fd_in < 0)
-		{
-			perror("Erreur ouverture fichier d'entrée");
-			exit(EXIT_FAILURE);
-		}
-		dup2(fd_in, STDIN_FILENO);
-		close(fd_in);
-	}
-	if (cmd->outfile)
-	{
-		int fd_out;
-		if (cmd->append)
-			fd_out = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		else
-			fd_out = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	char **envp;
+	char *cmd_path = data->path;
 
-		if (fd_out < 0)
-		{
-			perror("Erreur ouverture fichier de sortie");
-			exit(EXIT_FAILURE);
-		}
-		dup2(fd_out, STDOUT_FILENO);
-		close(fd_out);
-	}
+// Conversion de la liste des variables d'environnement en tableau
+    envp = env_list_to_array(data->copy_env);
+    if (!envp)
+    {
+        perror("Erreur de conversion des variables d'environnement");
+        free(cmd_path);
+        return;
+    }
+    // Gérer les redirections (entrée/sortie)
+    handle_redir_in_out(cmd);
+    // Exécuter la commande avec execve
+    if (execve(cmd_path, cmd->matrice, envp) == -1)
+    {
+        perror("Erreur execve");
+        free(cmd_path);
+        free(envp);
+        exit(EXIT_FAILURE);
+    }
+    free(cmd_path);
+    free(envp);
 }
-//apl execve pour executer les commandes externes
+
 void execve_cmd(t_data *data, t_cmd *cmd)
 {
-	(void)*data;
-	char *cmd_path = find_command_path(cmd->str);
-	if (!cmd_path)
+    // Trouver le chemin complet de la commande et le stocker dans `data->path`
+    data->path = find_command_path(cmd->matrice[0]);
+	printf ("valeur :%s\n", data->path);
+    if (!data->path)
+    {
+        printf("Commande non trouvée : %s\n", cmd->str);
+        return;
+    }
+
+    // Préparer les arguments pour execve et les stocker dans cmd->matrice
+    cmd->incmd = prepare_argv(cmd, data->path);
+    if (!cmd->incmd)
+    {
+        perror("Erreur de préparation des arguments");
+        free(data->path);
+        return;
+    }
+
+    // Gérer l'exécution et les redirections
+    exec_with_env_and_redir(cmd, data);
+}
+// gestion de l execution de commande soit directement soit via un pipe
+void	exec_cmd(t_data *data, t_cmd *cmd)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid == -1)
 	{
-		printf("Commande non trouvée : %s\n", cmd->str);
+		printf ("error fork");
 		return;
 	}
-	char *argv[] = {cmd_path, cmd->option, NULL};
-	char **envp;
-	
-	envp = NULL;
-	//gestion de redirection
-	handle_redir(cmd);
-	//execute la commande externe
-	if (execve(cmd_path, argv, envp) ==-1) //execute la commande externe
-	{              //ls  -la   NULL
-		perror("Erreur execve");  // Affiche l'erreur
-		exit(EXIT_FAILURE);  // Termine le processus enfant en cas d'échec
+	else if (pid == 0)
+	{
+		handle_redir_in_out(cmd);
+		execve_cmd(data, cmd);
+		perror("erreur d execution de commande");
+		exit(EXIT_FAILURE);
 	}
-	free(cmd_path);  // Libère la mémoire allouée pour le chemin de la commande
+	else
+	{
+		waitpid(pid, &status, 0);
+		data->last_exit_status = WEXITSTATUS(status);
+	}
 }
-
-//gere la creation du processus enfant av fork et exec la comm avec execve ds le processus enfant si ok
-bool	exec_cmd(t_data *data, t_cmd *cmd)
-{
-    pid_t pid;
-    int status; //recupere le code de retour
-
-    pid = fork();
-    if (pid < 0)
-    {
-        printf ("error fork");
-        return (false);
-    }
-    else if (pid == 0)
-        execve_cmd(data, cmd);
-    else
-    {
-        waitpid(pid, &status, 0); //recupere avec l& le status du processus fils
-        if (WIFEXITED(status)) //verif si fin et extrait le code retour
-        {
-            data->last_exit_status = WEXITSTATUS(status);//stock le code de retour
-        }
-    }
-    return (true);
-}
-
-//execution des commandes externes
-//gere la logique, verifie les commandes et prepare les pipes
+//gestion globale de l execution des commandes et des pipes
 bool	exec(t_data *data, t_cmd **cmd)
 {
 	t_cmd	*tmp;
@@ -134,5 +119,5 @@ bool	exec(t_data *data, t_cmd **cmd)
 		}
 		exec_cmd(data, tmp);
 	}
-	return (true);//exuec reussi
+	return (true);
 }
---
+++ src/exec/inout.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inout.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spagliar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 14:38:18 by spagliar          #+#    #+#             */
+/*   Updated: 2024/10/16 14:38:20 by spagliar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+//manipule les descripteurs de fichiers entree / sortie suivant 2 conditions
+void	handle_redir_in_out(t_cmd *cmd)
+{
+	int	fd_in;
+	int	fd_out;
+	printf("n\n");
+	if (cmd->infile)
+	{
+		//printf ("infile : %s\n", cmd->infile);
+		fd_in = open(cmd->infile, O_RDONLY);
+		printf ("infile open : %s\n", cmd->infile);
+		if (fd_in == -1)
+		{
+			perror("Erreur ouverture fichier d'entrée");
+			exit(EXIT_FAILURE);
+		}
+		dup2(fd_in, STDIN_FILENO);
+		close(fd_in);
+	}
+	if (cmd->outfile)
+	{
+		printf ("outfile : %s\n", cmd->outfile);
+		if (cmd->append)
+		{
+			fd_out = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+			printf ("outfile 1 : %s\n", cmd->outfile);
+		}
+		else
+		{
+			fd_out = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			printf ("outfile 2 : %s\n", cmd->outfile);
+		}
+		if (fd_out == -1)
+		{
+			perror("Erreur ouverture fichier de sortie");
+			exit(EXIT_FAILURE);
+		}
+		dup2(fd_out, STDOUT_FILENO);
+		close(fd_out);
+	}
+}
